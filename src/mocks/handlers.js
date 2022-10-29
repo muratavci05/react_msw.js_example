8c3edb44fdb3a30c259c53f6c8f53e4ae0fbadff
muratavci05 <js.muratavci@gmail.com>
2022-10-29 08:29:36 +0300

    mocks file in handlersJS add

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,2 +1,19 @@
 import { rest } from 'msw'
 
+export const handlers = [
+    // Handles a POST /login request
+    rest.post('/login', (req, res, ctx)=> {
+        
+        // Persist user's authentication in the session
+        // herhangibir request geldiğinde sessions'a is-authenticated değeri true olarak set ediliyor 
+        sessionStorage.setItem('is-authenticated', 'true')
+
+        return response(
+            // Respond with a 200 status code,(yanıt)
+            ctx.status(200),
+          )
+    }),
+
+    // Handles a GET /user request
+    rest.get('/user', null),
+  ]
